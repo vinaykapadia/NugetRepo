1f41a7154d5605f75e6c30a0b3341a87db8b4d98
Vinay Kapadia <dev@vkapadia.com>
2023-05-08 18:00:35 +0100

    Add project files.

---
+++ NugetRepo/App_Start/NuGetODataConfig.cs
@@ -0,0 +1,41 @@
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Routing;
+using NuGet.Server;
+using NuGet.Server.Infrastructure;
+using NuGet.Server.V2;
+using NugetRepo.App_Start;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NuGetODataConfig), nameof(NuGetODataConfig.Start))]
+
+namespace NugetRepo.App_Start 
+{
+    public static class NuGetODataConfig 
+    {
+        public static void Start() 
+        {
+            ServiceResolver.SetServiceResolver(new DefaultServiceResolver());
+
+            var config = GlobalConfiguration.Configuration;
+
+            config.UseNuGetV2WebApiFeed("NuGetDefault",
+                "nuget",
+                "PackagesOData",
+                enableLegacyPushRoute: true);
+
+            config.Services.Replace(typeof(IExceptionLogger), new TraceExceptionLogger());
+
+            // Trace.Listeners.Add(new TextWriterTraceListener(HostingEnvironment.MapPath("~/NuGet.Server.log")));
+            // Trace.AutoFlush = true;
+
+            config.Routes.MapHttpRoute(
+                name: "NuGetDefault_ClearCache",
+                routeTemplate: "nuget/clear-cache",
+                defaults: new { controller = "PackagesOData", action = "ClearCache" },
+                constraints: new { httpMethod = new HttpMethodConstraint(HttpMethod.Get) }
+            );
+
+        }
+    }
+}
